40c825cdb69ed730699474c8aff67770f022d265
youseop <edlsw@naver.com>
2021-03-13 16:29:19 +0900

    schema update + create make_game page

--- client/src/components/views/GameUploadPage/GameUploadPage2.js
+++ client/src/components/views/GameUploadPage/GameUploadPage2.js
@@ -1,51 +1,15 @@
 import React, { useState } from 'react'
-import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import { Typography, Button, Form, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
 import Axios from 'axios';
-import { useSelector } from 'react-redux';
 import './GameUploadPage2';
 
-const {TextArea} = Input
 const {Title} = Typography
 
-const PrivateOptions = [
-  { value: 0, label: "Private"},
-  { value: 1, label: "Public"}
-]
-
-const CategoryOptions = [
-  {value: 0, label: "Film & Animation"},
-  {value: 1, label: "Autos & Vehicles"},
-  {value: 2, label: "Music"},
-  {value: 3, label: "Pets & Animals"},
-]
-
 function GameUploadPage2(props) {
-  const user = useSelector(state => state.user);
-  const [videoTitle, setVideoTitle] = useState("")
-  const [description, setDescription] = useState("")
-  const [isPrivate, setIsPrivate] = useState(0)
-  const [category, setCategory] = useState("Film & Animation")
 
   const [filePath, setFilePath] = useState("")
-  // const [duration, setDuration] = useState("")
-  // const [thumbnailPath, setThumbnailPath] = useState("")
 
-  const onTitleChange = (event) => {
-    setVideoTitle(event.currentTarget.value)
-  }
-
-  const onDescriptionChange = (event) => {
-    setDescription(event.currentTarget.value)
-  }
-
-  const onPrivateChange = (event) => {
-    setIsPrivate(event.currentTarget.value)
-  }
-
-  const onCartegoryChange = (event) => {
-    setCategory(event.currentTarget.value)
-  }
 
   const onDrop = (files) => {
     if(!files[0]){
@@ -62,11 +26,6 @@ function GameUploadPage2(props) {
     Axios.post('/api/game/uploadfiles', formData, config)
     .then(response => {
       if(response.data.success) {
-        // console.log(response.data);
-        // let variable = {
-        //   url: response.data.url,
-        //   fileName: response.data.fileName
-        // }
         setFilePath(response.data.url)
 
       } else {
@@ -75,44 +34,13 @@ function GameUploadPage2(props) {
     })
   }
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(videoTitle==="" || description==="" || filePath===""){
-      alert("모든 정보를 입력해주세요.");
-      return
-    }
-    const variables = {
-      game_creater: user.userData._id,
-      game_title: videoTitle,
-      game_detail: description,
-      game_thumbnail: filePath,
-      game_privacy: isPrivate,
-      game_category: category,
-      game_writer: [],
-      game_character: [],
-      game_background: [],
-    }
-
-    Axios.post('/api/game/uploadgame', variables)
-    .then(response => {
-      if(response.data.success) {
-        message.success('첫 Scene을 생성해주세요. 오른쪽의 +버튼을 활용해 이미지들을 추가할 수 있습니다.');
-
-        setTimeout(() => {
-          props.history.push('/game/upload2');
-        },2000);
-      } else {
-        alert('업로드 실패')
-      }
-    })
-  }
 
   return (
     <div style={{ maxWidth:'700px', margin:'2rem auto'}}>
       <div style={{textAlign:'center', marginBottom:'2rem'}}>
-        <Title level={2}>Upload Video2</Title>
+        <Title level={2}>Make Game(..사진 추가 구현중)edit해도 상관없습니다.</Title>
       </div>
-      <Form onSubmit={onSubmit}>
+      <Form>
         <div style={{display:'flex', justifyContent:'space-between'}}>
           {/* drop zone */}
           <Dropzone
@@ -137,38 +65,11 @@ function GameUploadPage2(props) {
           }
 
         </div>
+
         <br/>
         <br/>
-        <label>Title</label>
-        <Input 
-          onChange={onTitleChange}
-          value={videoTitle}
-        />
-        <br/>
-        <br/>
-        <label>Description</label>
-        <TextArea 
-          onChange={onDescriptionChange}
-          value={description}
-        />
-        <br/>
-        <br/>
-        <select onChange={onPrivateChange}>
-          {PrivateOptions.map((item, index) => (
-            <option key={index} value={item.value}>{item.label}</option>
-          ))}
-        </select>
-        <br/>
-        <br/>
-        <select onChange={onCartegoryChange}>
-          {CategoryOptions.map((item, index) => (
-            <option key={index} value={item.value}>{item.label}</option>
-          ))}
-        </select>
-        <br/>
-        <br/>
-        <Button type="primary" size="large" onClick={onSubmit}>
-          Next Step
+        <Button type="primary" size="large">
+          Finish
         </Button>
 
       </Form>
