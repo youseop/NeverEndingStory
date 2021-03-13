df85f54d39be55fce8a8ec539121d194093690f3
stkang9409 <stkang9409@gmail.com>
2021-03-13 10:45:25 +0000

    add getscene router

--- server/routes/game.js
+++ server/routes/game.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Game } = require("../models/Game");
-
+const mongoose = require("mongoose");
 const { auth } = require("../middleware/auth");
 
 const multer = require("multer");
@@ -77,21 +77,60 @@ router.get('/getgames', (req, res) => {
 })
 
 
-router.get('/getscene', auth, async (req, res) => {
+router.get('/getscene/:id', auth, async (req, res) => {
+
+  // fakeScene = {
+  //   background_img: "/back1.png",
+  //   character_img: "/iu.png",
+  //   text: "사랑해요... 통키씨....",
+  // }
+  // return res.status(200).json({success:true, scene});
+
   // 로그인 중인 유저 가지고 오기..
-  const user_id = req.user._id;
-  // 로그인 중인 유저의 객체 가지고 오기
-  const user = await User.findOne({_id: user_id});
+  const gameId = mongoose.Types.ObjectId(req.params.id);
+  const userId = req.user._id;
+  // 로그인 중인 유저객체에서 진행중인 게임리스트 가지고 오기
+  try{
+    const user = await User.findOne({_id: userId});
+    const playingList = user.gameHistory;
+    // 지금 하려는 게임의 아이디가 진행중인 게임 리스트에 있는지 확인.
+    for(let i=0; i < playingList.length; i++)
+    {
+      const playingGame = playingList[i];
+      // 게임 아이디가 진행리스트에 있다면?
+      if (playingGame.gameId === gameId) {
+        // 리스트의 씬아이디로 씬 객체 찾아서 전달
+        const sceneId = playingGame.sceneId;
+        try{
+          const scene = await Scene.findOne({_id: sceneId});
+          return res.status(200).json({success:true, scene});
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  
+    // 못찾은 경우 내려옴
+    // 게임의 첫 씬 찾아서 전달
+    try {
+      const scene = await Game.findOne({_id: gameId}).game_first_scene;
+      return res.status(200).json({success:true, scene})
+    } catch (err) {
+      console.log(err);
+    }
+  } catch(err) {
+    console.log(err);
+  }
+)
 
-  user.
 
-  Game.find()
-    .populate('game_creater')
-    .exec((err, games) => {
-      if(err) return res.status(400).send(err);
-      res.status(200).json({success:true, games})
-    })
-})
+router.post('/updatescenestatus', auth, async (req, res) => {
+  if (!req.user) {
+    return res.status(200).json({success: false, msg: "Not a user"});
+  }
+  return;
+}
+
 
 
 router.post('/getgamedetail', (req, res) => {
