387f25909738264af5d07d0ed197eb811d38fe45
stkang9409 <stkang9409@gmail.com>
2021-03-14 07:51:20 +0000

    new

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,7 +15,7 @@ import Footer from "./views/Footer/Footer"
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
-
+ 
 function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
