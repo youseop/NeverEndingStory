a8acc37fdc24f6ae67069b73559d629c1d38e0ca
youseop <edlsw@naver.com>
2021-03-21 22:09:51 +0900

    rebase

--- client/src/components/views/GamePlayPage/CharacterBlock.js
+++ client/src/components/views/GamePlayPage/CharacterBlock.js
@@ -2,12 +2,14 @@ import "./CharacterBlock.css";
 import React from "react";
 const CharacterBlock = (props) => {
   const { characterList, onRemove_character } = props;
-  const position = [[35], [20, 50], [10, 60, 35]];
+  const positionX = [[35], [20, 50], [10, 35, 60]];
+  const positionY = [[10], [10, 20], [0, 15, 90]];
   const characterblocks = characterList.map((url, index) => {
     return (
       <img
         className="characterblock"
-        style={{ left: `${position[characterList.length - 1][index]}%` }}
+        style={{ left: `${positionX[characterList.length - 1][index]}%`,
+                 bottom: `${positionY[characterList.length - 1][index]}%` }}
         src={url}
         key={index}
         onClick={() => onRemove_character(index)}
