cf560246a978ad3661893c819c470b369f500176
stkang9409 <stkang9409@gmail.com>
2021-03-23 18:23:52 +0000

    fix timer clock

--- client/src/components/views/Modal/InputModal.js
+++ client/src/components/views/Modal/InputModal.js
@@ -7,6 +7,7 @@ import { useHistory } from "react-router";
 import { socket } from "../../App";
 import axios from "axios";
 import { loadEmptyNum } from "../../../_actions/sync_actions";
+import { SecurityScanTwoTone } from "@ant-design/icons";
 
 const InputModal = ({ scene_id, scene_depth, game_id, setClickable, scene_next_list }) => {
   const dispatch = useDispatch()
@@ -17,33 +18,34 @@ const InputModal = ({ scene_id, scene_depth, game_id, setClickable, scene_next_l
   const [visible, setVisible] = useState(false);
   const [formRef, setFormRef] = useState(null);
   const [remainTime, setRemainTime] = useState(0);
-  const [selecting, setSelecting] = useState(1);
   const [increaseTimer, setIncreaseTimer] = useState(null);
-  const [decreaseTimer, setDecreaseTimer] = useState(false);
+  const [decreaseTimer, setDecreaseTimer] = useState(null);
   const [validated, setValidated] = useState(1)
 
+  const tick = 30;
+
+
   const handleCreate = () => {
-    formRef.validateFields( async (err, values) => {
+    formRef.validateFields(async (err, values) => {
       if (err) {
         return;
       }
-
-      clearTimeout(increaseTimer);
       clearTimeout(decreaseTimer);
+      clearTimeout(increaseTimer);
 
       const data = {
         gameId: game_id,
         prevSceneId: scene_id,
-        isFirst : 0,
+        isFirst: 0,
         sceneDepth: scene_depth + 1,
         title: values.title,
       };
       const res = await axios.post("/api/scene/create", data);
       //! socket 보내서 서버에서 scene Cache x exp 업데이트
-      
-      socket.emit("created_choice", { sceneId: scene_id, userId: user_id, exp : res.data.exp })
+
+      socket.emit("created_choice", { sceneId: scene_id, userId: user_id, exp: res.data.exp })
       // tmp scene create
-      
+
       formRef.resetFields();
       setVisible(false);
 
@@ -63,11 +65,24 @@ const InputModal = ({ scene_id, scene_depth, game_id, setClickable, scene_next_l
 
   const onClickHandler = () => {
     clearTimeout(increaseTimer);
+    clearTimeout(decreaseTimer);
     setIncreaseTimer(setTimeout(() => {
       cancelHandler();
     }, 30000));
-    setRemainTime(29);
-    setSelecting(selecting * -1);
+
+    let tick = 30;
+    setRemainTime(tick);
+    const decTimer = setInterval(() => {
+      tick--;
+      if (tick === 0) {
+        clearInterval(decTimer);
+        cancelHandler();
+        return;
+      }
+      setRemainTime(tick);
+    }, 970);
+    setDecreaseTimer(decTimer);
+
     socket.emit("empty_num_decrease", { scene_id, user_id });
 
     return setVisible(true);
@@ -86,11 +101,11 @@ const InputModal = ({ scene_id, scene_depth, game_id, setClickable, scene_next_l
   }
 
   useEffect(() => {
-    socket.on("validated", (data) =>{
-      setValidated(validated*-1)
+    socket.on("validated", (data) => {
+      setValidated(validated * -1)
     })
 
-    socket.on("decrease_failed", ()=> {
+    socket.on("decrease_failed", () => {
       console.log("failed..");
       clearTimeout(increaseTimer);
       clearTimeout(decreaseTimer);
@@ -98,48 +113,40 @@ const InputModal = ({ scene_id, scene_depth, game_id, setClickable, scene_next_l
       setVisible(false);
     })
 
-    socket.emit("validate_empty_num", { scene_id})
+    socket.emit("validate_empty_num", { scene_id })
   }, [scene_id])
 
   const [working, setWorking] = useState();
   useEffect(() => {
-      console.log("en", emptyNum);
-      const nextListLen = Array.isArray(scene_next_list) ? scene_next_list.length : 0;
-      const workingCnt = nextListLen + emptyNum
-      console.log("nextListLen : ",nextListLen, "emptyNum : ",emptyNum)
-      if (workingCnt>=0 && workingCnt <= 4){
-        console.log("working Cnt : ",workingCnt)
-        setWorking([...Array(4 - workingCnt)].map((n, index) => {
-          return <div key={index}>작업중..<br /></div>
-        }))
-      }
+    console.log("en", emptyNum);
+    const nextListLen = Array.isArray(scene_next_list) ? scene_next_list.length : 0;
+    const workingCnt = nextListLen + emptyNum
+    console.log("nextListLen : ", nextListLen, "emptyNum : ", emptyNum)
+    if (workingCnt >= 0 && workingCnt <= 4) {
+      console.log("working Cnt : ", workingCnt)
+      setWorking([...Array(4 - workingCnt)].map((n, index) => {
+        return <div key={index}>작업중..<br /></div>
+      }))
+    }
   }, [emptyNum, scene_next_list])
 
 
-  useEffect(() => {
-    if (remainTime > 0) {
-      setDecreaseTimer(setTimeout(() => {
-        setRemainTime(remainTime - 1);
-      }, 980));
-    }
-  }, [remainTime, selecting])
-
   return (
     <>
-    {
-      working
-    }
+      {
+        working
+      }
       {
         emptyNum > 0 &&
-          <>
-            <div
-              id = "choice"
-              onClick={onClickHandler}
-              style={{ color: "red" }}
-            >
-              선택의 길...
+        <>
+          <div
+            id="choice"
+            onClick={onClickHandler}
+            style={{ color: "red" }}
+          >
+            선택의 길...
             </div>
-          </>
+        </>
       }
       <ModalForm
         ref={saveFormRef}
