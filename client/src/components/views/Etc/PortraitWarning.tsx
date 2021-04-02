0eac8ad6de6da9458d9ce0bf6e7a38810952bac7
stkang9409 <stkang9409@gmail.com>
2021-04-02 12:53:59 +0000

    add mobile portrait block

--- client/src/components/views/Etc/PortraitWarning.tsx
+++ client/src/components/views/Etc/PortraitWarning.tsx
@@ -3,7 +3,7 @@ import { Icon } from "antd";
 
 function PortraitWarning() {
   return (
-    <div >
+    <div className="portraitWarning">
       <p>
         가로 화면으로 즐겨주세요~ <Icon type="smile" />
       </p>
