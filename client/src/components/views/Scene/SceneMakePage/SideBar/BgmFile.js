6e925b15615c4c648774faa3f4fac5557d076d42
SeungHyun Kim <daejjyu@gmail.com>
2021-04-11 00:23:36 +0900

    update

--- client/src/components/views/Scene/SceneMakePage/SideBar/BgmFile.js
+++ client/src/components/views/Scene/SceneMakePage/SideBar/BgmFile.js
@@ -1,9 +1,16 @@
+import { message } from "antd";
 import React from "react";
 import { LOCAL_HOST } from "../../../../Config"
 
 function BgmFile({ bgm_audio, bgm, setBgmFile, setReload }) {
     let cutIdx = bgm_audio.src.lastIndexOf("/") + 1;
     let bgm_uri = decodeURI(bgm_audio.src.substr(cutIdx))
+    message.info(`${cutIdx}, ${bgm_audio.src}, ${bgm_uri}, ${bgm.music}, ${bgm.music.substr(cutIdx)}`)
+    console.log(123, cutIdx)
+    console.log(456, bgm_audio.src)
+    console.log(789, bgm_uri)
+    console.log(999, bgm.music)
+    console.log(1000, bgm.music.substr(cutIdx))
     const onClick_music = () => {
         if (bgm_uri === bgm.music.substr(cutIdx)) {
             bgm_audio.src = ""
